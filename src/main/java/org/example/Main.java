441a48ebb9fd514b28f099f7d85acfdc59c5eae7
ZeroFairy <jordanjv453@gmail.com>
2025-03-21 02:21:58 +0700

    Main class. Masih bisa di improve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import NaiveBayes.CalcBelong;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("File name in Resource folder : ");
+        String filename = sc.nextLine();
+        sc.close();
+
+        CalcBelong calc = new CalcBelong(filename);
+
+        Map<String, String> input = new HashMap<String, String>();
+        input.put("age", "31...40");
+        input.put("income", "low");
+        input.put("student", "no");
+        input.put("credit_rating", "fair");
+
+        calc.belong(input);
     }
 }
