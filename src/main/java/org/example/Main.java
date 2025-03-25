b30704f051d691cfa79c1807c27fa230238bcc99
ZeroFairy <jordanjv453@gmail.com>
2025-03-25 09:47:45 +0700

    adjust so that the user can change which feature gonna use as the class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,19 +12,29 @@ public class Main {
 
         System.out.println("File name in Resource folder : ");
         String filename = sc.nextLine();
-        sc.close();
 
-        CalcBelong calc = new CalcBelong(filename);
+        System.out.println("Feature for classification : ");
+        String feature = sc.nextLine();
 
         Map<String, String> input = new HashMap<String, String>();
+        CalcBelong calc = new CalcBelong(filename, feature);
+
+//        Scanner scan = new Scanner(System.in);
+//        for (String header : calc.getHeader()) {
+//            System.out.println("Nilai untuk feature " + header + " : ");
+//            String in = scan.next();
+//            input.put(header, in);
+//        }
+
         //naivebayes.csv
 //        input.put("age", "31...40");
-//        input.put("income", "low");
-//        input.put("student", "no");
-//        input.put("credit_rating", "fair");
+        input.put("income", "low");
+        input.put("student", "no");
+        input.put("credit_rating", "fair");
+        input.put("buys_computer", "yes");
 
         //iris.csv
-        input.put("SepalLength", "7");
+//        input.put("SepalLength", "7");
 //        input.put("SepalWidth", "3.5");
 //        input.put("PetalLength", "5.3");
 //        input.put("PetalWidth", "0.2");
