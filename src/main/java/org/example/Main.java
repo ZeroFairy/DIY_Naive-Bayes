af5b9d4b842ef8168e5391ed0618e55ae2c41d36
ZeroFairy <jordanjv453@gmail.com>
2025-03-26 20:01:51 +0700

    Adjust so that the user only need to input the needed value from terminal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,80 @@
+/**
+ * Author by Jordan Vincent
+ * Universitas Sanata Dharma
+ *
+ * Tugas untuk menentukan data inputan (X) merupakan golongan
+ * dari class yang mana sesuai dengan data yang ada
+ *
+ * An assignment for determining which
+ * class the given input (X) belongs to
+ *
+ * The given input
+ * age = "31...40"
+ * income = "low"
+ * student = "no"
+ * credit_rating = "fair"
+ */
 package org.example;
 
 import NaiveBayes.CalcBelong;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //Absolute path
         System.out.println("File name in Resource folder : ");
         String filename = sc.nextLine();
 
-        System.out.println("Feature for classification : ");
-        String feature = sc.nextLine();
+        String feature;
+        do {
+            System.out.println("\nAll Available Feature : " + getHeader(filename));
+            System.out.println("Feature for classification : ");
+            feature = sc.nextLine();
+        } while (!getHeader(filename).contains(feature));
 
         Map<String, String> input = new HashMap<String, String>();
         CalcBelong calc = new CalcBelong(filename, feature);
 
-//        Scanner scan = new Scanner(System.in);
-//        for (String header : calc.getHeader()) {
-//            System.out.println("Nilai untuk feature " + header + " : ");
-//            String in = scan.next();
-//            input.put(header, in);
-//        }
+        //Header without feature
+        List<String> header = new LinkedList<>();
+        for (String head : calc.getHeader()) {
+            if (!head.equalsIgnoreCase(feature)) {
+                header.add(head);
+            }
+        }
 
-        //naivebayes.csv
-//        input.put("age", "31...40");
-        input.put("income", "low");
-        input.put("student", "no");
-        input.put("credit_rating", "fair");
-        input.put("buys_computer", "yes");
+        for (String head : header) {
+            List<String> features = calc.getFeatures(head);
+            String label;
+            do {
+                System.out.println("\nFeature : " + head);
+                System.out.println("Please enter the label : " + features);
+                label = sc.nextLine();
+            } while (!features.contains(label));
 
-        //iris.csv
-//        input.put("SepalLength", "7");
-//        input.put("SepalWidth", "3.5");
-//        input.put("PetalLength", "5.3");
-//        input.put("PetalWidth", "0.2");
+            input.put(head, label);
+        }
 
         calc.belong(input);
     }
+
+    public static List<String> getHeader(String fileName) {
+        List<String> label = new LinkedList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line = br.readLine();
+
+            //Untuk nama column
+            String[] values = line.split("[,; ]+");
+            label = Arrays.stream(values).toList();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return label;
+    };
 }
