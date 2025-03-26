7aded0c5bcaac5fe9aa2c1957d20791e701231e4
ZeroFairy <jordanjv453@gmail.com>
2025-03-26 20:03:07 +0700

    Added the explanation for each important method (still have manual debug)

--- src/main/java/NaiveBayes/CalcBelong.java
+++ src/main/java/NaiveBayes/CalcBelong.java
@@ -11,6 +11,9 @@ public class CalcBelong {
     private List<String> header;
     private String feature;
 
+    /**
+     * constructor
+     */
     public CalcBelong(String fileName, String feature) {
         ReadCSV readCSV = new ReadCSV();
         readCSV.readCSVFile(fileName, feature);
@@ -19,39 +22,47 @@ public class CalcBelong {
         this.header = readCSV.getHeader();
     }
 
+    /**
+     * To calculate the probability results of each feature
+     * and determine the belonging
+     *
+     * @param input     --> all feature that are use for calculate the probability of belonging
+     */
     public void belong(Map<String, String> input) {
         Map<String, Map<String, Integer>> classColumn = this.metadata.get(feature);
         Map<String, List<Double>> probResult = this.calcAllFeature(input);
         Map<String, Double> labelResult = new HashMap<>();
 
-        System.out.println("\nBELONG");
-        System.out.println("Prob Result : " + probResult);
+        System.out.println("\nBELONGING");
+//        System.out.println("Prob Result : " + probResult);
 
         int totalLine = this.countTotalLine(classColumn);
-        System.out.println("Total Line : " + totalLine);
+//        System.out.println("Total Line : " + totalLine);
 
         int counter = 0;
         for (Map.Entry<String, Map<String, Integer>> classLabel : classColumn.entrySet()) {
             for (Map.Entry<String, Integer> label : classLabel.getValue().entrySet()) {
                 double result = 1;
                 for (Map.Entry<String, List<Double>> prob : probResult.entrySet()) {
-                    System.out.println("\nProb get value : " + prob.getValue().get(counter));
+//                    System.out.println("\nProb get value : " + prob.getValue().get(counter));
 
                     //P(X|Ci)
                     result *= (double) prob.getValue().get(counter);
                 }
-                System.out.println("Label Key : " + label.getKey());
-                System.out.println("Class Label Key : " + classLabel.getKey());
-                System.out.println("Result : " + result);
-                System.out.println("Label Value : " + label.getValue());
+//                System.out.println("Label Key : " + label.getKey());
+//                System.out.println("Class Label Key : " + classLabel.getKey());
+//                System.out.println("Result : " + result);
+//                System.out.println("Label Value : " + label.getValue());
+
+                System.out.println("CLASS " + classLabel.getKey() + " --> P(X|Ci) : " + result);
+
                 //P(X|Ci) * P(Ci)
                 labelResult.put(classLabel.getKey(), result * ((double) label.getValue() / totalLine));
 
-//                counter++;
-                counter-=-1;
+                counter-=-1;        //counter++;
             }
         }
-        System.out.println("Label Result : " + labelResult);
+        System.out.println("\nP(X|Ci) * P(Ci) : " + labelResult);
 
         this.greatestProb(labelResult);
     }
@@ -66,9 +77,12 @@ public class CalcBelong {
             }
         }
 
-        System.out.println("Thus, X belongs to class (" + belongsLabel + " = " + max + ")");
+        System.out.println("Thus, X belongs to class (" + belongsLabel + " = " + max + ") IN feature " + this.feature);
     }
 
+    /**
+     * Count the total line of the dataset
+     */
     public int countTotalLine(Map<String, Map<String, Integer>> classColumn) {
         int result = 0;
         for (Map.Entry<String, Map<String, Integer>> classLabel : classColumn.entrySet()) {
@@ -81,6 +95,8 @@ public class CalcBelong {
     }
 
     /**
+     * Calculate every probability of each feature in the dataset
+     *
      * @param input --> Map of feature and value, from the search
      */
     public Map<String, List<Double>> calcAllFeature(Map<String, String> input) {
@@ -93,21 +109,25 @@ public class CalcBelong {
             probResult.put(entry.getKey(), featProb);
         }
 
-        System.out.println("Prob Result: " + probResult);
+//        System.out.println("Prob Result: " + probResult);
         return probResult;
     }
 
     /**
+     * Calculate the probability of each label in the class feature
+     * against the feature
+     *
      * @param feature --> column
-     * @param value   --> one of the value inside the column
+     * @param value   --> one of the label inside the class feature
      */
     public List<Double> calcEachLabelInValue(String feature, String value) {
         List<Double> result = new LinkedList<Double>();
         Map<String, Map<String, Integer>> classColumn = this.metadata.get(this.header.getLast());
 
+        System.out.println("\nFeature " + feature);
         for (Map.Entry<String, Map<String, Integer>> classLabel : classColumn.entrySet()) {
             for (Map.Entry<String, Integer> label : classLabel.getValue().entrySet()) {
-                System.out.println("\nEach label " + feature + " --> " + value + " : " + label.getKey() + " || " + label.getValue());
+//                System.out.println("\nEach label " + feature + " --> " + value + " : " + label.getKey() + " || " + label.getValue());
 
                 result.add(this.calcFeatureProb(feature, value, label.getKey(), label.getValue()));
             }
@@ -118,27 +138,39 @@ public class CalcBelong {
 
     //untuk 1 class label
     /**
+     * Calculate probability of label against the feature
+     *
      * @param feature --> column
-     * @param value   --> one of the value inside the column
+     * @param value   --> one of the label inside the class feature
      * @param label   --> class clasification
      */
     public double calcFeatureProb(String feature, String value, String label ,int labelValue) {
         Map<String, Map<String, Integer>> column = this.metadata.get(feature);
 
-        System.out.println("CALC FEATURE PROB");
-        System.out.println("Column.get(value).get(label) : " + column.get(value).get(label));
-        System.out.println("Label Value: " + labelValue);
-        System.out.println("Column.size() : " + column.size());
+//        System.out.println("CALC FEATURE PROB");
+//        System.out.println("Column.get(value).get(label) : " + column.get(value).get(label));
+//        System.out.println("Label Value: " + labelValue);
+//        System.out.println("Column.size() : " + column.size());
 
         //column + 1 / jumlah label + jumlah value di column
         double result = (double) ((column.get(value).get(label) == null ? 0 : column.get(value).get(label)) + 1) / (labelValue + column.size());
 
-        System.out.println("PROBABILITY : " + result);
+        System.out.println("PROBABILITY OF " + value + " --> " + label + " ARE : " + result);
 
         return result;
     }
 
     public List<String> getHeader() {
         return header;
+    }
+
+    public List<String> getFeatures(String feature) {
+        List<String> result = new LinkedList<>();
+
+        for (Map.Entry<String, Map<String, Integer>> classLabel : this.metadata.get(feature).entrySet()) {
+            result.add(classLabel.getKey());
+        }
+
+        return result;
     }
 }
--- src/main/java/NaiveBayes/ReadCSV.java
+++ src/main/java/NaiveBayes/ReadCSV.java
@@ -8,28 +8,35 @@ import java.io.*;
 import java.util.*;
 
 public class ReadCSV {
-    /* Untuk menyimpan jumlah dari setiap bentuk isian untuk setiap column */
+    /* Untuk menyimpan jumlah dari setiap bentuk isian untuk setiap kolom */
+    /* To store the number of each form of input for each column */
     private LinkedHashMap<String, Map<String, Map<String, Integer>>> metadata;
     private List<String> header;
 
+    /**
+     * constructor
+     */
     public ReadCSV() {
         this.metadata = new LinkedHashMap<String, Map<String, Map<String, Integer>>>();
         this.header = new LinkedList<>();
     }
 
+    /**
+     * Read the CSV file and the only keep the important data inside attribute metadata
+     *
+     * The Important Data are the count of each feature label against each label in class feature
+     *
+     * @param fileName  --> the name of the dataset file
+     * @param feature   --> the feature that are using for classification
+     */
     public void readCSVFile(String fileName, String feature) {
-//        InputStream inputStream = ReadCSV.class.getClassLoader().getResourceAsStream(fileName);
-//        String filePath = ReadCSV.class.getClassLoader().getResource(fileName).getPath();
-//        System.out.println("File path: " + filePath);
-
-//        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line = br.readLine();
 
-            //Untuk nama column
+            //For each column feature
             String[] values = line.split("[,; ]+");
             for (String value : values) {
-                //Belum ada column
+                //If it haven't got the feature
                 if (!metadata.containsKey(value)) {
                     Map<String, Map<String, Integer>> column = new HashMap<>();
                     metadata.put(value, column);
@@ -38,36 +45,34 @@ public class ReadCSV {
                 }
             }
 
-            System.out.println("HEADER: " + header);
+//            System.out.println("HEADER: " + header);
 
             int index = header.indexOf(feature);
 
-            //Setiap baris
+            //Each line
             while ((line = br.readLine()) != null) {
                 values = line.split("[,; ]+");
-//                Iterator i = header.iterator();
                 int counter = 0;
 
-                //Setiap column
+                //each column
                 for (String value : values) {
-                    System.out.println("\nMETADATA READ CSV " + counter + " : " + this.metadata);
+//                    System.out.println("\nMETADATA READ CSV " + counter + " : " + this.metadata);
                     Map<String, Map<String, Integer>> column = this.metadata.get(header.get(counter));
-//                    Map<String, Map<String, Integer>> column = this.metadata.get(i.next());
 
                     Map<String, Integer> label = new HashMap<>();
-                    //Belum ada value dari column
+                    //Haven't got the label of the column
                     if (!column.containsKey(value)) {
                         label.put(values[index], 1);
 
                         column.put(value, label);
-                    //Sudah ada value
+                    //Already got the label of the column
                     } else {
-                        System.out.println("Value : " + value);
+//                        System.out.println("Value : " + value);
                         label = column.get(value);
 
                         if (label.containsKey(values[index])) {
-                            System.out.println("Prev Label : " + label);
-                            System.out.println("Class Label : " + values[index]);
+//                            System.out.println("Prev Label : " + label);
+//                            System.out.println("Class Label : " + values[index]);
                             label.put(values[index], label.get(values[index]) + 1);
                             column.put(value, label);
                         } else {
@@ -76,15 +81,14 @@ public class ReadCSV {
                     }
 
                     this.metadata.put(header.get(counter), column);
-                    System.out.println("OUT METADATA READ CSV " + counter + " : " + this.metadata);
+//                    System.out.println("OUT METADATA READ CSV " + counter + " : " + this.metadata);
 
-                    //counter++
-                    counter-=-1;
+                    counter-=-1;    //counter++
                 }
             }
 
-            System.out.println("Read CSV");
-            System.out.println("METADATA" +this.metadata);
+//            System.out.println("Read CSV");
+//            System.out.println("METADATA" +this.metadata);
         } catch (IOException e) {
             e.printStackTrace();
         }
